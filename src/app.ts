3fd01a6ad65b86c22c833f463dc872ab6f68f992
BOSS <boss@MacBook-Air-BOSS.local>
2020-12-02 21:47:17 +0200

    feat: first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import * as http from 'http';
+import * as express from 'express';
+//import  Router  from './controllers/router';
+import Middleware from './middleware';
+
+
+class Server {
+    private app: express.Application;
+
+    constructor() {
+        const app: express.Application = express();
+        Middleware.init(app);
+        //Router.init(app);
+        app.set('port', process.env.PORT || 3000);
+        this.app = app;
+    }
+    public start(): http.Server {
+        return this.app.listen(this.app.get('port'), () => {
+            console.log(`server is listening on port: ${this.app.get('port')}`);
+        });
+    }
+}
+
+ new Server().start();

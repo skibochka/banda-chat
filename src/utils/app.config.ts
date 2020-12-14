e1a357817d6621836dfc409ee922652af41dad2f
Alex Boiko <arekisanda@protonmail.com>
2020-12-14 20:49:02 +0200

    refactor(app.config.ts): added cors module

--- src/utils/app.config.ts
+++ src/utils/app.config.ts
@@ -2,6 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
 import * as helmet from 'helmet';
+import * as cors from 'cors';
 import * as express from 'express';
 
 class Config {
@@ -11,6 +12,7 @@ class Config {
     app.use(cookieParser());
     app.use(compression());
     app.use(helmet());
+    app.use(cors());
     app.set('port', process.env.PORT || 3000);
   }
 }
